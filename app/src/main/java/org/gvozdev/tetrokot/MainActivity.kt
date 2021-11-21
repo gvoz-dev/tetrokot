1fa8ac6dc144cc1cd143becbc2444d4b90f781fe
Roman Gvozdev <rsgvozdev@outlook.com>
2021-11-21 22:46:20 +0300

    Add main screen

--- app/src/main/java/org/gvozdev/tetrokot/MainActivity.kt
+++ app/src/main/java/org/gvozdev/tetrokot/MainActivity.kt
@@ -1,38 +1,101 @@
 package org.gvozdev.tetrokot
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import org.gvozdev.tetrokot.ui.theme.TetrokotTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TetrokotTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Tetrokot")
-                }
-            }
+            MainScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainScreen() {
+    Column {
+        Title(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1.0f)
+        )
+        MainMenu(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(6.0f)
+        )
+    }
+}
+
+@Composable
+fun Title(modifier: Modifier) {
+    val backgroundColor = Color(0xFF512DA8)
+    val textColor = Color(0xFFB3E5FC)
+    Box(modifier.background(backgroundColor), Alignment.Center) {
+        Text(
+            text = "Tetrokot",
+            color = textColor,
+            fontSize = 36.sp,
+            fontFamily = FontFamily.Serif,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Composable
+fun MainMenu(modifier: Modifier) {
+    val backgroundColor = Color(0xFFB3E5FC)
+    val activity = LocalContext.current as? Activity
+    Column(
+        modifier.background(backgroundColor),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        MenuButton("Start") { /*TODO*/ }
+        MenuButton("Leaderboard") { /*TODO*/ }
+        MenuButton("Settings") { /*TODO*/ }
+        MenuButton("Exit") { activity?.finish() }
+    }
+}
+
+@Composable
+fun MenuButton(text: String, onClick: () -> Unit) {
+    val backgroundColor = Color(0xFF512DA8)
+    val textColor = Color(0xFFB3E5FC)
+    Button(
+        onClick,
+        modifier = Modifier
+            .width(340.dp)
+            .height(120.dp)
+            .padding(10.dp),
+        colors = ButtonDefaults.buttonColors(backgroundColor, textColor),
+        shape = RoundedCornerShape(10.dp)
+    ) {
+        Text(text, fontSize = 30.sp, fontFamily = FontFamily.Serif)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    TetrokotTheme {
-        Greeting("Preview")
-    }
+    MainScreen()
 }
